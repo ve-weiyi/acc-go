c56e3f8bf0b93ef366e0ca7ba4bbd29b899843ea
waynie <waynie.wei@anker-in.com>
2022-11-15 14:27:33 +0800

    [修复] main文件改动bug

--- main.go
+++ main.go
@@ -1,34 +1,81 @@
+// 1.包名：包名一般与目录相同,相同目录只能有一个包名
 package main
 
+//2.导包：可以使用 import "p1" 和 import ("p1","p2" )
 import (
+	"acc/config"
+	"acc/lib/logger"
+	"acc/lib/orm"
+	"acc/lib/redis"
+	"acc/routers"
+	"context"
 	"fmt"
-	"reflect"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-type Person struct {
-	Name string `json:"name" from:"name" msg:"名字必填"`
-	Age  int    `json:"age" from:"age" msg:"年龄必填"`
-}
+// 3.init先于main运行
+func init() {
+	fmt.Println("init invoke")
 
-func (this Person) GetName() string {
-	return this.Name
-}
+	config.Setup()
 
-func (this *Person) SetName(name string) {
-	this.Name = name
-}
-
-func main() {
-	p := Person{
-		Name: "小明",
-		Age:  20,
+	if config.AppConfig.MysqlState {
+		orm.Setup()
 	}
-	v := reflect.ValueOf(&p)
-	var params []reflect.Value
-	params = append(params, reflect.ValueOf("李四"))
-	b := v.MethodByName("SetName").Call(params)
-	fmt.Println(b)
-	a := v.MethodByName("GetName").Call(nil)
-	fmt.Println(a)
-	fmt.Printf("%#v", p)
+
+	if config.AppConfig.RedisState {
+		redis.Setup()
+	}
+
+	if config.AppConfig.LoggerState {
+		logger.Setup()
+	}
+
+}
+
+// 4.只有main包下的main才能运行
+func main() {
+	fmt.Println("main invoke")
+	// 退出关闭 mysql 连接
+	if config.AppConfig.MysqlState {
+		defer orm.CloseDB()
+	}
+
+	port := fmt.Sprintf(":%s", config.AppConfig.Port)
+	router := routers.InitRouter()
+	readTimeout := config.AppConfig.ReadTimeout
+	writeTimeout := config.AppConfig.WriteTimeout
+	maxHeaderBytes := 1 << 20
+
+	server := &http.Server{
+		Addr:           port,
+		Handler:        router,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+
+	go func() {
+		// 服务连接
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("监听: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("关闭服务器...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalln("服务器关闭:", err)
+	}
+	log.Println("服务器已关闭")
 }
